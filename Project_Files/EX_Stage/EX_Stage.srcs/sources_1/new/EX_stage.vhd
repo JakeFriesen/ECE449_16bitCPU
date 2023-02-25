8aade991618497aef776c5b118ca0479e461ff41
Jake Friesen <jake.fries@hotmail.com>
2023-02-24 20:46:01 -0800

    Added default value to N, Z flags

--- Project_Files/EX_Stage/EX_Stage.srcs/sources_1/new/EX_stage.vhd
+++ Project_Files/EX_Stage/EX_Stage.srcs/sources_1/new/EX_stage.vhd
@@ -64,7 +64,7 @@ signal ALU_A, ALU_B, IR: std_logic_vector( 15 downto 0);
 signal ALU_OP: std_logic_vector (2 downto 0);
 signal ALU_result, Vdata, ALU_v_result, result: std_logic_vector(15 downto 0);
 signal z, n, v, v_en: std_logic;
-signal z_output, n_output: std_logic;
+signal z_output, n_output: std_logic := '0';
 signal data_sel: std_logic;    
 signal B_data, imm_data: std_logic_vector(15 downto 0);
 signal OPCODE: std_logic_vector(6 downto 0);
